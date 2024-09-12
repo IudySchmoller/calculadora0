684ffe49881bf73e6295d5b56497339d7defec7c
IudySchmoller <159266787+IudySchmoller@users.noreply.github.com>
2024-09-12 12:13:56 -0300

    12/09

---
+++ script.js
@@ -0,0 +1,5 @@
+function calculadora()
+    const num1 = parseFloat(document.getElementById('zeroum').value);
+    const num2 = parseFloat(document.getElementById('zerodois').value);
+    const num3 = parseFloat(document.getElementById('zerotres').value);
+    let delta = (b*b) * (-4 * a * c)
