29a2344368080b654cead2116a39b26caf3d7795
IudySchmoller <159266787+IudySchmoller@users.noreply.github.com>
2024-09-16 09:33:09 -0300

    16/08

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-function calculadora()
-    const a = parseFloat(document.getElementById("val_a").value);
-    const b = parseFloat(document.getElementById("val_b").value);
-    const c = parseFloat(document.getElementById("val_c").value);
+function calcula(){
+    const a = parseFloat(document.getElementById('val_a').value);
+    const b = parseFloat(document.getElementById('val_b').value);
+    const c = parseFloat(document.getElementById('val_c').value);
 
     const delta = (b*b) - (4 * a * c);
     const raiz_delta = Math.sqrt(delta);
@@ -9,13 +9,15 @@ function calculadora()
     let x2;
 
     if(delta < 0){
-        document.getElementById('res_1')innertext = não há raizes reais;
+        document.getElementById('res_1').innerText = "não há raizes reais";
     }else if(delta > 0){
         x1 = (-b + raiz_delta)/(2*a);
         x2 = (-b - raiz_delta)/(2*a);
-        document.getElementById('res_1')innertext = "valor de x1" = + x1;
-        document.getElementById('res_2')innertext = "valor de x2" = + x2;
-    }else{
+        document.getElementById('res_1').innerText = "valor de x1" + x1;
+        document.getElementById('res_2').innerText = "valor de x2" + x2;
+    }else{  
         x1 = (-b)/(2*a);
-        document.getElementById('res_1')innertext = "o valor da única raiz é" = + x1;
+        document.getElementById('res_1').innerText = "o unico valor da raiz é  " + x1;
     }
+
+}    
